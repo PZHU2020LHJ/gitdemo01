6cfd4fb44e19b7385cfd859872dde0b4117cc7bb
PZHU2020LHJ <2844661081@qq.com>
2022-10-17 12:01:05 +0800

    hello git 1

--- gitdemo/src/main/java/cn/phzu2020lhj/gidemo/Application.java
+++ gitdemo/src/main/java/cn/phzu2020lhj/gidemo/Application.java
@@ -3,5 +3,6 @@ package cn.phzu2020lhj.gidemo;
 public class Application {
     public static void main(String[] args) {
         System.out.printf("hello git 1");
+        System.out.printf("hello git 2");
     }
 }
