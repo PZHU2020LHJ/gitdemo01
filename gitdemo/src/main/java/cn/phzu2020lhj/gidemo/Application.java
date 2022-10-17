fb41b5ad48d4db6344d75378b15ceaa720ff6aa6
PZHU2020LHJ <2844661081@qq.com>
2022-10-17 15:29:15 +0800

    b1 hb 1

--- gitdemo/src/main/java/cn/phzu2020lhj/gidemo/Application.java
+++ gitdemo/src/main/java/cn/phzu2020lhj/gidemo/Application.java
@@ -4,5 +4,8 @@ public class Application {
     public static void main(String[] args) {
         System.out.printf("hello git 1");
         System.out.printf("hello git 2");
+        System.out.printf("hello git 3");
+        System.out.printf("hello git 4");
+        System.out.printf("hello git 5");
     }
 }
